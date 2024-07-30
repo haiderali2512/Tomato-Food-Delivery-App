8c37130ed7353dbd8e8cf806b9d8a452825a0e7d
haiderali2512 <haiderali251203@gmail.com>
2024-07-30 19:22:31 +0530

    updated Navbar

--- front-end/src/components/Navbar/Navbar.jsx
+++ front-end/src/components/Navbar/Navbar.jsx
@@ -16,10 +16,10 @@ const Navbar = ({setShowLogin}) => {
         <img src = {assets.logo} alt=""   className='logo'/>
       </Link>
       <ul className="navbar-menu">
-        <Link to='/' onClick={()=>setMenu("home")}className={menu==="home"?"active":""}>home</Link>
-        <a href='#explore-menu' onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>menu</a>
-        <a href='#app-download' onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>mobile-app</a>
-        <a href='#footer' onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>contact us</a>
+        <Link to='/' onClick={()=>setMenu("home")}className={menu==="home"?"active":""}>Home</Link>
+        <a href='#explore-menu' onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>Menu</a>
+        <a href='#app-download' onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>Mobile App</a>
+        <a href='#footer' onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>Contact Us</a>
       </ul>
       <div className='navbar-right'>
         <img src={assets.search_icon} alt="" />
@@ -27,7 +27,7 @@ const Navbar = ({setShowLogin}) => {
           <Link to="/cart"><img src={assets.basket_icon} alt="" /></Link>
           <div className={getTotalCartAmount() === 0?"": "dot"}></div>
         </div>
-        <button onClick={()=>setShowLogin(true)}>sign in</button>
+        <button onClick={()=>setShowLogin(true)}>Sign in</button>
       </div>
     </div>
   )
